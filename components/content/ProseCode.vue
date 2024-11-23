97bba1eacece0b4dbd44ee0fe3e41e6b45a66c6d
llayz46 <layzer@outlook.fr>
2024-11-23 11:59:40 +0100

    Custom tag design (ProseCode)

---
+++ components/content/ProseCode.vue
@@ -0,0 +1,15 @@
+<script setup>
+const active = ref(false)
+
+const style = computed(() => {
+    return active.value ? 'border-my-primary-700/50 bg-my-primary-700/10' : 'border-white/10 bg-my-background/25 hover:bg-my-background/50'
+})
+</script>
+
+<template>
+    <span class="border transition-colors px-2 py-1 text-sm rounded-full cursor-pointer leading-8"
+          :class="style"
+          @click="active = !active">
+        <code><slot /></code>
+    </span>
+</template>
