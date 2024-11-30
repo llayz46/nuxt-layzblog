898aa03f58b60a20fd405201be8a790fe92cdeb8
llayz46 <layzer@outlook.fr>
2024-11-30 11:47:39 +0100

    Fix: image in article subtitle path

--- components/content/Subtitle.vue
+++ components/content/Subtitle.vue
@@ -10,5 +10,5 @@
         <span v-for="tag in tags.slice(0, 11)" class="border border-white/10 bg-my-background/25 px-2 py-1 text-sm rounded-full cursor-default">{{ tag }}</span>
     </div>
 
-    <img :src="`../img/content/articles/${img}`" class="size-full object-cover aspect-video rounded-3xl shadow-2xl" alt="Image de couverture l'article">
+    <img :src="`/img/content/articles/${img}`" class="size-full object-cover aspect-video rounded-3xl shadow-2xl" alt="Image de couverture l'article">
 </template>
