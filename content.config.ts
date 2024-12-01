07d622d717e8319bab7c227f59ce8a9c6062071c
llayz46 <layzer@outlook.fr>
2024-12-01 22:30:47 +0100

    feat: creating new collections for projects and create first project

--- content.config.ts
+++ content.config.ts
@@ -3,15 +3,25 @@ import {z} from "zod";
 
 export const collections = {
     articles: defineCollection({
-        // Load every file inside the `content` directory
         source: {
             include: 'articles/*.md',
             prefix: '/blog'
         },
-        // Specify the type of content in this collection
         type: 'page',
         schema: z.object({
             date: z.string(),
+            title: z.string(),
+            description: z.string(),
+            draft: z.boolean(),
+        })
+    }),
+    projects: defineCollection({
+        source: {
+            include: 'projects/*.json',
+            prefix: '/portfolio'
+        },
+        type: 'page',
+        schema: z.object({
             title: z.string(),
             description: z.string(),
             draft: z.boolean(),
