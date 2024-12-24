a421b7d5da214734c668eb4ad349b623e0db776a
llayz46 <layzer@outlook.fr>
2024-12-24 19:59:07 +0100

    style: fix position of search modal on mobile

--- components/Search.vue
+++ components/Search.vue
@@ -82,7 +82,7 @@ const highlightedResults = computed(() => {
                 <div class="fixed inset-0 bg-gray-800/40 transition-opacity"/>
             </TransitionChild>
 
-            <div class="fixed inset-0 z-10 w-screen overflow-y-auto p-4 sm:p-6 md:p-20">
+            <div class="fixed max-md:top-14 inset-0 z-10 w-screen overflow-y-auto p-4 sm:p-6 md:p-20">
                 <TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0 scale-95"
                                  enter-to="opacity-100 scale-100" leave="ease-in duration-200"
                                  leave-from="opacity-100 scale-100" leave-to="opacity-0 scale-95">
